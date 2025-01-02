b35129369f7ca7928c076d251a2c9deca28bcb93
Nusrat Jahan Raisa <jahanthinks@gmail.com>
2025-01-02 22:02:41 +0600

    moving into another html page

---
+++ js/index.js
@@ -0,0 +1,16 @@
+//step -1 add event handler
+//step-2 prevent default behavior(reloading)
+//step-3 get specific element by id
+
+document.getElementById('login-button').addEventListener('click', function(event){
+   event.preventDefault();
+
+   const phoneNumber =document.getElementById('phone-number').value;
+   const pin =document.getElementById('pin-number').value
+//    console.log(phoneNumber,pin)
+
+   if( phoneNumber==='01820033756' && pin==='111' ){
+      
+       window.location.href = 'homePage.html'
+   }
+})
